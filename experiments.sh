0e17d4d247fdba9aa6a56e58f317eb4e49049675
HarukiENdo <e.haruki@alps@lab.org>
2024-05-20 14:02:36 +0900

    実験条件の追加

--- experiments.sh
+++ experiments.sh
@@ -2,17 +2,18 @@ experiment=$1
 if [ ! -d ./experiment/$experiment ]; then
     mkdir -p ./experiment/$experiment
 fi
-# for lr in 0.0005 0.005; do
-#     for n_mels in 30 40; do
-#         for window_size in 30 40; do
-#             python train.py --wandb --learning_rate $lr --n_mels $n_mels --window_size $window_size --project_name $experiment &> $experiment/train_lr${lr}_nmels${n_mels}_ws${window_size}.log &
-#             wait
-#         done
-#     done
-# done
 
-for model in cnn_network1 cnn_network2 cnn_network3 cnn_network4 resnet18; do
-    python train.py --wandb --loss focal_loss --model $model --project_name $experiment &> $experiment/train_${model}_focal_loss.log &
-    wait
+#実験条件の追加
+for loss in cross_entropy focal_loss; do
+    for optimizer in  adam adamw; do
+        for model in cnn_network4 resnet18 resnet34 resnet50; do
+            for learning_rate in 0.00005 0.0005; do
+                for window_size in 15 40 90; do
+                    CUDA_VISIBLE_DEVICES=0 python train.py --wandb --loss $loss --optimizer $optimizer --model $model --learning_rate $learning_rate --window_size $window_size --project_name $project_name &> experiment/$project_name/train_${model}_${loss}_${optimizer}_${learing_rate}_${window_size}.log &
+                    wait
+                done
+            done
+        done
+    done
 done
 
