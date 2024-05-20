b6679f2d9b049a0658b9065fc535a05d639c7377
HarukiENdo <e.haruki@alps@lab.org>
2024-05-20 16:50:45 +0900

    実験条件の追加

--- experiments2.sh
+++ experiments2.sh
@@ -12,7 +12,7 @@ for loss in cross_entropy focal_loss; do
         for model in cnn_network4 resnet18 resnet34 resnet50; do
             for learning_rate in 0.00005 0.0005; do
                 for window_size in 15 40 90; do
-                    CUDA_VISIBLE_DEVICES=0 python train.py --wandb --loss $loss --optimizer $optimizer --model $model --learning_rate $learning_rate --window_size $window_size --project_name $project_name &> experiment/$project_name/train_${model}_${loss}_${optimizer}_${learing_rate}_${window_size}.log &
+                    CUDA_VISIBLE_DEVICES=0 python train.py --wandb --loss $loss --optimizer $optimizer --model $model --learning_rate $learning_rate --window_size $window_size --project_name $project_name &> experiment/$project_name/train_${model}_${loss}_${optimizer}_${learning_rate}_${window_size}.log &
             
                     # 同時に実行するプロセス数を制限
                     if [ $(jobs | wc -l) -ge $max_parallel_processes ]; then
