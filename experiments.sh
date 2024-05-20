1986e31c70c90b0f49c11a178b174cd867d34ba4
HarukiENdo <e.haruki@alps@lab.org>
2024-05-20 16:50:36 +0900

    実験条件の追加

--- experiments.sh
+++ experiments.sh
@@ -1,6 +1,6 @@
-experiment=$1
-if [ ! -d ./experiment/$experiment ]; then
-    mkdir -p ./experiment/$experiment
+project_name=$1
+if [ ! -d ./experiment/$project_name ]; then
+    mkdir -p ./experiment/$project_name
 fi
 
 #実験条件の追加
@@ -9,7 +9,7 @@ for loss in cross_entropy focal_loss; do
         for model in cnn_network4 resnet18 resnet34 resnet50; do
             for learning_rate in 0.00005 0.0005; do
                 for window_size in 15 40 90; do
-                    CUDA_VISIBLE_DEVICES=0 python train.py --wandb --loss $loss --optimizer $optimizer --model $model --learning_rate $learning_rate --window_size $window_size --project_name $project_name &> experiment/$project_name/train_${model}_${loss}_${optimizer}_${learing_rate}_${window_size}.log &
+                    CUDA_VISIBLE_DEVICES=0 python train.py --wandb --loss $loss --optimizer $optimizer --model $model --learning_rate $learning_rate --window_size $window_size --project_name $project_name &> experiment/$project_name/train_${model}_${loss}_${optimizer}_${learning_rate}_${window_size}.log &
                     wait
                 done
             done
